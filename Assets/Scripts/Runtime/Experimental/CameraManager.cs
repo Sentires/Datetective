9daaaebbba629fb5741bf6054df08253c034a9a9
Sen <45946529+Sentires@users.noreply.github.com>
2026-05-04 20:48:07 +0800

    Camera System Changes
    New camera manager & various tweaks to the existing assets & scripts.

---
+++ Assets/Scripts/Runtime/Experimental/CameraManager.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TheDates.Runtime.General;
+using TheDates.Runtime.PlayerCore;
+using UnityEngine;
+using UnityEngine.U2D;
+
+namespace TheDates
+{
+    public class CameraManager : BasicSingleton<CameraManager>
+    {
+        public event Action<RenderState> OnCameraStateChange = delegate { };
+        public enum RenderState { Disabled, PixelWorld, HighDefWorld }
+
+        [field: SerializeField, ReadOnly] public RenderState currentState { get; private set; } = RenderState.Disabled;
+        
+        [SerializeField] private Camera2DHandler pixelWorldCamera;
+        [SerializeField] private Camera2DHandler highDefWorldCamera;
+        //[SerializeField] private Camera uiCamera;
+        [SerializeField] private RenderState defaultState = RenderState.PixelWorld;
+
+        public Dictionary<RenderState, Camera2DHandler> cameraMap { get; private set; }
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            cameraMap = new Dictionary<RenderState, Camera2DHandler>
+            {
+                { RenderState.Disabled, null },
+                { RenderState.PixelWorld, pixelWorldCamera },
+                { RenderState.HighDefWorld, highDefWorldCamera }
+            };
+            pixelWorldCamera.AssertNull("Camera Manager");
+            highDefWorldCamera.AssertNull("Camera Manager");
+            //uiCamera.AssertNull("Camera Manager");
+            
+            SetState(defaultState);
+        }
+
+        public void SetState(RenderState newState) {
+            if (currentState == newState) return;
+            currentState = newState;
+            
+            pixelWorldCamera.enabled = currentState == RenderState.PixelWorld;
+            highDefWorldCamera.enabled = currentState == RenderState.HighDefWorld;
+            
+            OnCameraStateChange(currentState);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Runtime/PlayerCore/Camera2DHandler.cs
@@ -0,0 +1,116 @@
+using System;
+using TheDates.Runtime.General;
+using TheDates.Runtime.Input;
+using UnityEngine;
+
+namespace TheDates.Runtime.PlayerCore
+{
+    [RequireComponent(typeof(Camera))]
+    public class Camera2DHandler : MonoBehaviour
+    {
+        [SerializeField] private Vector2 offset;
+        [SerializeField] private float damping = 0.5f;
+        [SerializeField] private bool snapOnEnable = true;
+        
+        public Transform target;
+        public Vector2 trackingDeadZone;
+        
+        private Vector3 _velocity;
+        private Vector3 _totalOffset;
+        private bool _followTarget => pairedCamera != null;
+
+        [field: SerializeField, ReadOnly] public Camera pairedCamera { get; private set; }
+        //[field: SerializeField, ReadOnly] public float defaultScale { get; private set; }
+        
+        public bool isFollowingTarget => _followTarget && target != null;
+
+        private void Awake() {
+            pairedCamera = GetComponent<Camera>();
+            //defaultScale = pairedCamera.orthographicSize;
+        }
+
+        private void Start() {
+            
+            _totalOffset = new Vector3(offset.x, offset.y, transform.position.z);
+            //InputContextChanged(GameEventsManager.Instance.InputEvents.inputEventContext); // double check now
+
+            var startPosition = isFollowingTarget && snapOnEnable ? target.position : transform.position;
+            
+            startPosition.z = 0f;
+            transform.position = startPosition + _totalOffset;
+        }
+
+        private void OnEnable() {
+            pairedCamera ??= GetComponent<Camera>();
+            pairedCamera.enabled = true;
+            
+            
+            //if (!GameEventsManager.HasInstance) return;
+            //GameEventsManager.Instance.InputEvents.onInputContextChange += InputContextChanged;
+            
+        }
+
+        private void OnDisable() {
+            pairedCamera ??= GetComponent<Camera>();
+            pairedCamera.enabled = false;
+            
+            //if (!GameEventsManager.HasInstance) return;
+            //GameEventsManager.Instance.InputEvents.onInputContextChange -= InputContextChanged;
+        }
+        
+        
+
+        //private void InputContextChanged(InputEvents.Context contextType) {
+        //        _followTarget = contextType == InputEvents.Context.World;
+        //}
+        
+        public void SetOffset(Vector2 newOffset) {
+            offset = newOffset;
+            _totalOffset = new Vector3(offset.x, offset.y, _totalOffset.z);
+        }
+
+        private void LateUpdate() {
+            if (!isFollowingTarget) return;
+            
+            //var cameraPos = PixelSnap(transform.position);
+            //var targetPos = PixelSnap(target.position + _totalOffset);
+            var cameraPos = transform.position;
+            var targetPos = target.position + _totalOffset;
+            targetPos.z = _totalOffset.z;
+            
+            
+            if (IsWithinDeadZone(targetPos, cameraPos)) {
+                transform.position = Vector3.SmoothDamp(cameraPos, targetPos, ref _velocity, damping);
+                //transform.position = PixelSnap(Vector3.SmoothDamp(cameraPos, targetPos, ref _velocity, damping));
+                //Vector3Int.
+            }
+        }
+
+        private bool IsWithinDeadZone(Vector3 to, Vector3 from) {
+            return trackingDeadZone == Vector2.zero || 
+                   Mathf.Abs(to.x - from.x) > trackingDeadZone.x ||
+                   Mathf.Abs(to.y - from.y) > trackingDeadZone.y;
+        }
+        
+        // This just visualises the offset and deadzone for us
+        #if UNITY_EDITOR
+        private void OnDrawGizmosSelected() {
+            var position = transform.position;
+            
+            if (trackingDeadZone != Vector2.zero) {
+                Gizmos.color = Color.blue;
+                
+                var gizmoArea = new Vector3(trackingDeadZone.x, trackingDeadZone.y, position.z);
+                Gizmos.DrawWireCube(position, gizmoArea);
+            }
+            if (offset != Vector2.zero) {
+                Gizmos.color = Color.green;
+            
+                var gizmoOffset = new Vector3(offset.x, offset.y, 0f);
+                gizmoOffset += position;
+                Gizmos.DrawLine(position, gizmoOffset);
+            }
+        }
+        #endif
+    }
+}
