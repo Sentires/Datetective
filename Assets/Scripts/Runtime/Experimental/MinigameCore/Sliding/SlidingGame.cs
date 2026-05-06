a693508da91d7787f65e0ab58578941d8c64eadc
Sen <45946529+Sentires@users.noreply.github.com>
2026-05-06 23:43:20 +0800

    Refactoring Sliding Puzzle
    Prefab unchanged, but general cleaning up and integration with the newer manager iteration. I did add a proper win condition for now, but I expect to change it before Sunday.
    
    There's probably ways to improve the logic later.

--- Assets/Scripts/Runtime/Experimental/MinigameCore/Sliding/SlidingGame.cs
+++ Assets/Scripts/Runtime/Experimental/MinigameCore/Sliding/SlidingGame.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using TheDates.Runtime;
 using TheDates.Runtime.Experimental.MinigameCore;
 using TheDates.Runtime.General;
@@ -9,53 +10,57 @@ using UnityEngine;
 
 namespace TheDates
 {
-    public class SlidingGame : MonoBehaviour//, IMiniGame
+    public class SlidingGame : MiniGame
     {
-        [Header("Exposed Fields")]
-        [SerializeField, ReadOnly] private MiniGameManager miniGameManager;
-        [field: SerializeField, ReadOnly] public bool isInitialised { get; private set; }
-        [field: SerializeField, ReadOnly] public MiniGameState gameState { get; private set; }
-        
-        private HashSet<int> _collidables = new();
-        private Transform[,] tiles;
-        
+        [Header("Mechanic Config")]
         public List<GameObject> pieces = new();
         public Transform pieceParent;
-        public Vector2Int gridSize = new Vector2Int(3, 3);
+        public Vector2Int gridSize = new(3, 3);
+        
+        [Header("Exposed Fields")]
+        [SerializeField, ReadOnly] private MiniGameManager miniGameManager;
+        [SerializeField, ReadOnly] private bool initialised;
+        [SerializeField, ReadOnly] private GameObject prefab;
+        [SerializeField, ReadOnly] private MiniGameState state;
+        public override bool isInitialised => initialised;
+        public override GameObject source => prefab;
+        public override MiniGameState gameState => state;
+        
+        private readonly HashSet<int> _collidables = new();
+        private Transform[,] _tiles;
+        private Vector2Int _currentIndex;
+        private Transform _currentTile;
 
-        private Vector2Int currentIndex;
-        private Transform currentTile;
-
-        private void Start()
-        {
-            //var length = Mathf.CeilToInt((float)pieces.Count / 2);
-            tiles = new Transform[gridSize.x, gridSize.y];
-            //for (var i = 0; i < pieces.Count; i++)
-            //{
-                //var id = pieces[i].gameObject.GetInstanceID();
-                
-                
-                //_collidables.Add(id);
-                //Debug.Log($"ADDED piece {pieces[i].name}({id}) at index [{i}], sibling index is [{pieces[i].transform.GetSiblingIndex()}]");
-                //CreatePiece(i + 1, pieces[i]);
-                //ArrangePiece()
-            //}
-    
+        private void SetupGame() {
+            _currentIndex = Vector2Int.zero;
+            _currentTile = null;
+            SetupTiles();
+        }
+        
+        private void SetupTiles() {
             var index = 0;
-            //for (var y = 0; y < gridSize.y; y++)
-            for (var y = gridSize.y - 1; y >= 0; y--)
-            {
-                for (var x = 0; x < gridSize.x; x++)
-                {
-                    //if (!pieces.IsWithinBounds(index)) break;
+            for (var y = gridSize.y - 1; y >= 0; y--) {
+                for (var x = 0; x < gridSize.x; x++) {
+                    var piece = pieces[index];
+                    _tiles[x,y] = piece.transform;
+                    ArrangePiece(new Vector3(x, y, -1), piece);
+                    index++;
+                }
+            }
+        }
+
+        private void GenerateTiles() {
+            _tiles = new Transform[gridSize.x, gridSize.y];
+            
+            var index = 0;
+            for (var y = gridSize.y - 1; y >= 0; y--) {
+                for (var x = 0; x < gridSize.x; x++) {
                     GameObject piece;
                     var isWithinBounds = index < pieces.Count;
-                    //int id;
+
                     if (!isWithinBounds || pieces[index] == null) {
                         piece = new GameObject($"Empty Tile");
                         piece.transform.SetParent(pieceParent);
-                        //piece.transform.SetAsLastSibling();
-                        //piece.transform.SetSiblingIndex(index);
                         
                         if (isWithinBounds) {
                             pieces[index] = piece;
@@ -63,48 +68,36 @@ namespace TheDates
                         else {
                             pieces.Add(piece);
                         }
-                        
-                    }
-                    else {
+                    } else {
                         piece = pieces[index];
-                        if (!piece.transform.IsChildOf(pieceParent))
-                        {
-                            Debug.Log($"IGNORED piece {piece.name} at index [{index}], sibling index is [{piece.transform.GetSiblingIndex()}]");
+                        if (!piece.transform.IsChildOf(pieceParent)) {
+                            //Debug.Log($"IGNORED piece {piece.name} at index [{index}], sibling index is [{piece.transform.GetSiblingIndex()}]");
                             pieces[index].SetActive(false);
                             continue;
                         }
                         piece.name = $"Tile";
-                        
                     }
-                    
-                    CreatePiece(piece.transform.GetSiblingIndex(), piece);
-                    tiles[x,y] = piece.transform;
+                    var siblingIndex = piece.transform.GetSiblingIndex();
+                    CreatePiece(siblingIndex, piece);
                     _collidables.Add(piece.GetInstanceID());
-                    
-                    Debug.Log($"ADDED piece {piece?.name}({x}, {y}) at index [{index}], sibling index is [{piece?.transform.GetSiblingIndex()}]");
-                    
-                    ArrangePiece(new Vector3(x, y, -1), piece);
                     index++;
                 }
             }
         }
 
-        private void OnEnabled()
-        {
-            miniGameManager.OnClickPosition += OnPointPosition;
-            miniGameManager.OnClickState += OnClickState;
-            miniGameManager.OnClickTarget += OnClickTarget;
+        private void OnEnabled() {
+            if (!MiniGameManager.HasInstance) return;
+            
+            MiniGameManager.Instance.OnClickTarget += OnClickTarget;
         }
 
-        private void OnDisabled()
-        {
-            miniGameManager.OnClickPosition -= OnPointPosition;
-            miniGameManager.OnClickState -= OnClickState;
-            miniGameManager.OnClickTarget -= OnClickTarget;
+        private void OnDisabled() {
+            if (!MiniGameManager.HasInstance) return;
+            
+            MiniGameManager.Instance.OnClickTarget -= OnClickTarget;
         }
 
-        private void CreatePiece(int index, GameObject piece)
-        {
+        private void CreatePiece(int index, GameObject piece) {
             var text = piece.GetComponentInChildren<TextMeshPro>();
             if (text != null) {
                 text.text = index.ToString();
@@ -115,132 +108,135 @@ namespace TheDates
             piece.SetActive(false);
         }
 
-        private void ArrangePiece(Vector3 position, GameObject piece)
-        {
+        private void ArrangePiece(Vector3 position, GameObject piece) {
             piece.transform.localPosition = position;
+            piece.transform.localScale = Vector3.one;
             piece.SetActive(true);
         }
         
-        private void OnClickTarget(RaycastHit2D input)
-        {
-            //throw new NotImplementedException();
+        private void OnClickTarget(RaycastHit2D input) {
             var target = input.transform;
             if (!_collidables.TryGetValue(target.gameObject.GetInstanceID(), out var wire)) return;
             Debug.Log($"Clicked {input.collider?.gameObject.name}");
             
-            
-            //var index = Vector2Int.zero;
-            //for (var y = 0; y < gridSize.y; y++)
-            if (currentTile == null) SelectTile(target);
-            else
-            {
+            if (_currentTile == null) SelectTile(target);
+            else {
                 SwapTile(target);
-                
+                if (!CheckScore()) return;
+                if (_currentTile) _currentTile.localScale = Vector3.one;
+                _currentTile = null;
+                SendCommand(MiniGameCommand.Win);
             }
-            //Debug.Log($"OnClickTarget Any called with {input.collider.name}");
-            //if ( != null)
-            //if (!_collidables.TryGetValue(input.collider.GetInstanceID(), out var wire)) return;
-            //if (wire.clickPoint != input.collider) return;
-            //_currentWire = wire;
-            //Debug.Log($"OnClickTarget called with {input.collider.name} : {wire.name}");
         }
 
-        private void SelectTile(Transform target)
-        {
-            for (var y = gridSize.y - 1; y >= 0; y--)
-            {
-                for (var x = 0; x < gridSize.x; x++)
-                {
-                    if (tiles[x, y] == target)
-                    {
-                        currentTile = target;
-                        currentIndex = new Vector2Int(x, y);
-                        currentTile.localScale = SelectedScale;
-                    }
+        private void SelectTile(Transform target) {
+            for (var y = gridSize.y - 1; y >= 0; y--) {
+                for (var x = 0; x < gridSize.x; x++) {
+                    if (_tiles[x, y] != target) continue;
+                    
+                    _currentTile = target;
+                    _currentIndex = new Vector2Int(x, y);
+                    _currentTile.localScale = SelectedScale;
                 }
             }
         }
 
         private static readonly Vector3 SelectedScale = new(0.8f, 0.8f, 1f);
 
-        private void SwapTile(Transform target)
-        {
+        private void SwapTile(Transform target) {
             //var canSwap = true;
             Vector2Int newIndex;
-            if (currentIndex.x - 1 >= 0 && target == tiles[currentIndex.x - 1, currentIndex.y]) {
-                newIndex = new Vector2Int(currentIndex.x - 1, currentIndex.y);
-            }
-            else if (currentIndex.x + 1 < gridSize.x && target == tiles[currentIndex.x + 1, currentIndex.y]) {
-                newIndex = new Vector2Int(currentIndex.x + 1, currentIndex.y);
-            }
-            else if (currentIndex.y - 1 >= 0 && target == tiles[currentIndex.x, currentIndex.y - 1]) {
-                newIndex = new Vector2Int(currentIndex.x, currentIndex.y - 1);
-            }
-            else if (currentIndex.y + 1 < gridSize.y && target == tiles[currentIndex.x, currentIndex.y + 1]) {
-                newIndex = new Vector2Int(currentIndex.x, currentIndex.y + 1);
-            }
-            else
-            {
+            if (_currentIndex.x - 1 >= 0 && target == _tiles[_currentIndex.x - 1, _currentIndex.y]) {
+                newIndex = new Vector2Int(_currentIndex.x - 1, _currentIndex.y);
+            } else if (_currentIndex.x + 1 < gridSize.x && target == _tiles[_currentIndex.x + 1, _currentIndex.y]) {
+                newIndex = new Vector2Int(_currentIndex.x + 1, _currentIndex.y);
+            } else if (_currentIndex.y - 1 >= 0 && target == _tiles[_currentIndex.x, _currentIndex.y - 1]) {
+                newIndex = new Vector2Int(_currentIndex.x, _currentIndex.y - 1);
+            } else if (_currentIndex.y + 1 < gridSize.y && target == _tiles[_currentIndex.x, _currentIndex.y + 1]) {
+                newIndex = new Vector2Int(_currentIndex.x, _currentIndex.y + 1);
+            } else {
                 Debug.Log($"Target {target.gameObject.name} is out of range.");
-                currentTile.localScale = Vector3.one;
-                currentTile = null;
+                _currentTile.localScale = Vector3.one;
+                _currentTile = null;
                 return;
             }
 
             // Jetbrains suggested deconstruction instead of x = y, y = z, z = x, but it looks cursed lmao
-            (currentTile.localPosition, target.localPosition) = (target.localPosition, currentTile.localPosition);
+            (_currentTile.localPosition, target.localPosition) = (target.localPosition, _currentTile.localPosition);
             
-            //currentTile.localScale = Vector3.one;
-            tiles[newIndex.x, newIndex.y] = currentTile;
-            //target.localScale = SelectedScale;
-            tiles[currentIndex.x, currentIndex.y] = target;
+            _tiles[newIndex.x, newIndex.y] = _currentTile;
+            _tiles[_currentIndex.x, _currentIndex.y] = target;
             
-            //currentTile = target;
-            currentIndex = newIndex;
-
-            //if ((currentIndex.x - 1 >= 0 && target == tiles[currentIndex.x - 1, currentIndex.y])
-            //    || (currentIndex.x + 1 < gridSize.x && target == tiles[currentIndex.x + 1, currentIndex.y])
-            //    || (currentIndex.y - 1 >= 0 && target == tiles[currentIndex.x, currentIndex.y - 1])
-            //    || (currentIndex.y + 1 < gridSize.y && target == tiles[currentIndex.x, currentIndex.y + 1]))
-            //{
-            //    Debug.Log($"Swapped {target?.name}");
-            //}
+            _currentIndex = newIndex;
         }
 
-        private void OnClickState(bool input)
-        {
-            //if (_currentWire == null) return;
+        private bool CheckScore() {
+            var index = 0;
+            for (var y = gridSize.y - 1; y >= 0; y--) {
+                for (var x = 0; x < gridSize.x; x++) {
+                    if (_tiles[x, y].GetSiblingIndex() != index) {
+                        return false;
+                    }
+                    index++;
+                }
+            }
+            Debug.Log("You win!!!");
+            return true;
             
-            //_currentWire = null;
         }
-        private void OnPointPosition(Vector2 input)
-        {
-            //if (_currentWire == null) return;
-            //_movePosition = miniGameManager.GetWorldSpacePosition(input, -0.3f);
-            //_currentWire.clickPoint.transform.position = _movePosition;
-            //_currentWire.MoveWire(_movePosition);
-        }
-        [field: SerializeField, ReadOnly] public GameObject source { get; private set; }
-        public void Init(GameObject prefab)
-        {
+        
+        public override void Init(GameObject gamePrefab) {
             if (!MiniGameManager.HasInstance || MiniGameManager.Instance == miniGameManager) return;
             miniGameManager = MiniGameManager.Instance;
-            source = prefab;
+            prefab = gamePrefab;
             
+            GenerateTiles();
+        }
+        
+        public override void AcceptCommand(MiniGameCommand command) {
+            var newState = command switch {
+                MiniGameCommand.Start => StartGame(),
+                MiniGameCommand.Reset => ResetGame(),
+                MiniGameCommand.Win => WinGame(),
+                MiniGameCommand.Lose => LoseGame(),
+                _ => QuitGame() // ForceQuit here
+            };
+            
+            SetState(newState);
+        }
+        
+        private void SetState(MiniGameState newState, bool notifyManager = true) {
+            if (state == newState) return;
+            
+            state = newState;
+            if (notifyManager) MiniGameManager.Instance.NotifyMiniGameState(source, state);
+        }
+        
+        private MiniGameState WinGame() {
+            Debug.Log("Yippee I finished!");
+            QuitGame();
+            return MiniGameState.Completed;
         }
 
-        public void StartGame()
-        {
+        private MiniGameState LoseGame() {
+            Debug.Log("Aw shucks, got it wrong. Let me try again!");
+            return ResetGame();
+        }
+
+        private MiniGameState StartGame() {
             OnEnabled();
+            SetupGame();
+            return MiniGameState.Active;
         }
 
-        public void ResetGame()
-        {
+        private MiniGameState ResetGame() {
+            SetupGame();
+            return MiniGameState.Active;
         }
 
-        public void StopGame()
-        {
+        private MiniGameState QuitGame() {
             OnDisabled();
+            return MiniGameState.Inactive;
         }
     }
 }
